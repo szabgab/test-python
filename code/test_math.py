4f2cd34cd861d97b90661b022d30f500de26d2c7
Gabor Szabo <gabor@szabgab.com>
2017-01-27 13:31:07 +0200

    print some system details

--- code/test_math.py
+++ code/test_math.py
@@ -1,10 +1,16 @@
 import unittest
+import sys, os
 import code.math
 
 class TestCod(unittest.TestCase):
     def test_main(self):
+        print(sys.version_info)
+        print(sys.platform)
+        print(sys.executable)
+        print(os.uname())
         self.assertEqual(code.math.add(1, 2), 3)
 
 #    def test_ohter(self):
 #        self.assertEqual(code.math.add(1, 1, 1), 3)
 
+# vim: expandtab
