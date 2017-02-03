0cd7ace115e9014dc9ce617afd9c7c80aafec2c3
Gabor Szabo <gabor@szabgab.com>
2017-02-03 15:02:20 +0200

    add test in main directory

---
+++ example.py
@@ -0,0 +1,3 @@
+import code.mymath
+
+print(code.mymath.add(2, 3))
---
+++ tests/test_me.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import code.mymath
+class TestMe:
+    def test_add(self):
+        assert code.mymath.add(2, 3) == 5
+
+    def test_example(self, tmpdir):
+        path = str(tmpdir.join('abc'))
+        os.system('python example.py > {}'.format(path))
+        with open(path) as fh:
+            result = fh.read()
+        assert result.rstrip("\n") == "5"
