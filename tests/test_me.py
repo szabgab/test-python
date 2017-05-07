597b1795864729dfcd7c989e49dc95f20b6dc7f0
Gabor Szabo <gabor@szabgab.com>
2017-05-07 17:57:38 +0300

    another test case

--- tests/test_me.py
+++ tests/test_me.py
@@ -3,8 +3,11 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import my.code
 
 class TestMe:
-    def test_add(self):
+    def test_is_anagram(self):
         assert my.code.is_anagram("abc", "cba")
+
+    def test_is_multiword_anagram(self):
+        assert my.code.is_anagram("a b c", "c b a")
 
     def test_example(self, tmpdir):
         path = str(tmpdir.join('abc'))
